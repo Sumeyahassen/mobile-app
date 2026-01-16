5ca61111385e223106ec4f850bd13a3b88564bbc
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-16 22:24:09 +0300

    to learn how to easely install  vite and to integerate the latest virsion of tailwind css  and integeration

---
+++ react/rt/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+const App=()=>{
+  return (
+    <>
+<h1 className='text-red-400'>Sumeya</h1>
+    </>
+  )
+}
+
+export default App
