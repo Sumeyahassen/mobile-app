d8efe5100d83d262de837f544ff2a1b6e55cd61d
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-16 22:49:41 +0300

    to applay the pading futer for the text

--- react/rt/src/App.jsx
+++ react/rt/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 const App=()=>{
   return (
     <div className='bg-amber-300'>
-<h1 className='text-red-400 text-xl font-extrabold'>Sumeya</h1>
+<h1 className='text-red-400 text-xl font-extrabold px-10 py-12'>Sumeya</h1>
 <p className='px-10 py-5 bg-rose-600 mx-11'></p>
     </div>
   )
