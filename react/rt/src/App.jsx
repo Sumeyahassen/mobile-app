54c9c1d9cdab02efbce558ff60b9d8f0591a451b
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-16 23:02:07 +0300

    how to select colore and  for the dark and light mode time  easely to select colore for example for light mode to use the  50 for bg and 900 for dark

--- react/rt/src/App.jsx
+++ react/rt/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 const App=()=>{
   return (
-    <div className='bg-amber-300'>
-<h1 className='text-red-400 text-xl font-extrabold px-10 py-12'>Sumeya</h1>
-<p className='px-10 py-5 bg-rose-600 mx-11'></p>
-<h1 className='pt-5 pb-10 pl-18 pr-16 text-blue-600 mr-10 bg-rose-300'>Hello world</h1>
-    </div>
+//     <div className='bg-amber-300'>
+// <h1 className='text-red-400 text-xl font-extrabold px-10 py-12'>Sumeya</h1>
+// <p className='px-10 py-5 bg-rose-600 mx-11'></p>
+// <h1 className='pt-5 pb-10 pl-18 pr-16 text-blue-600 mr-10 bg-rose-300'>Hello world</h1>
+//     </div>
+<div className='bg-blue-50 min-h-screen'>
+
+</div>
   )
 }
 
