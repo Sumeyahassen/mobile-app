22289e96019df53cab166cb50dfa1939722624a6
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-16 22:35:47 +0300

    to paractes the  text property to use  i able to change the position and to use the font property to use abllee to  boldenes

--- react/rt/src/App.jsx
+++ react/rt/src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 const App=()=>{
   return (
     <>
-<h1 className='text-red-400'>Sumeya</h1>
+<h1 className='text-red-400 text-xl font-extralight'>Sumeya</h1>
     </>
   )
 }
