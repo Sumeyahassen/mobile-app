59d4a8a98198a8c9a1855301fbf9323048b0d371
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-16 22:45:07 +0300

    to learn about coloring

--- react/rt/src/App.jsx
+++ react/rt/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 const App=()=>{
   return (
-    <>
-<h1 className='text-red-400 text-xl font-extralight'>Sumeya</h1>
-    </>
+    <div className='bg-amber-300'>
+<h1 className='text-red-400 text-xl font-extrabold'>Sumeya</h1>
+<p className='p-10 bg-rose-600 mx-11'></p>
+    </div>
   )
 }
 
