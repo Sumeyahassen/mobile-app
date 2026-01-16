fe2bad1f55a3b081cf771eb67b133ef7e40aec36
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-16 22:53:03 +0300

    to add the padding  for diffrent diraction

--- react/rt/src/App.jsx
+++ react/rt/src/App.jsx
@@ -4,6 +4,7 @@ const App=()=>{
     <div className='bg-amber-300'>
 <h1 className='text-red-400 text-xl font-extrabold px-10 py-12'>Sumeya</h1>
 <p className='px-10 py-5 bg-rose-600 mx-11'></p>
+<h1 className='pt-5 pb-10 pl-18 pr-16'>Hello world</h1>
     </div>
   )
 }
