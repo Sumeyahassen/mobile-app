7af126815b7d5f76bfa771903f3bddcbfe6293bc
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-24 20:57:26 +0300

    to complate the header html text form

---
+++ react/rt/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      {/* logo */}
+      <div className="">React Class</div>
+      {/* navbar */}
+      <nav>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Servise</a>
+        <a href="">Projects</a>
+        <a href="">Contact</a>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
