67321d9ba8ff4dafc8ad641bd707c35dc7b9e2d9
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-24 21:36:19 +0300

    the menu is fully response if click it  respons to include the javascript code

--- react/rt/src/components/Header.jsx
+++ react/rt/src/components/Header.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Header() {
+    const [isMenu,setMenu]=useState(false);
+    const open=()=>{
+        setMenu(!isMenu);
+    }
   return (
     <div className='flex justify-between items-center bg-amber-600 h-16 px-5'>
       {/* logo */}
       <div className="">React Class</div>
-      <div className="sm:hidden">X</div>
+      <div className="sm:hidden cursor-pointer" onClick={open}>{isMenu?"X":"Menu"}</div>
       {/* navbar */}
       <nav className='hidden sm:flex justify-between gap-4'>
         <a href="">Home</a>
@@ -14,13 +18,15 @@ function Header() {
         <a href="">Projects</a>
         <a href="">Contact</a>
       </nav>
-      <nav className='flex flex-col justify-between gap-4 min-h-min sm:hidden absolute top-16 left-1/2 pt-4 bg-amber-400 '>
+        {/* mobile menu */}
+        {isMenu && <nav className='flex flex-col justify-between gap-4 min-h-min sm:hidden absolute top-16 left-1/2 pt-4 bg-amber-400 '>
         <a href="">Home</a>
         <a href="">About</a>
         <a href="">Servise</a>
         <a href="">Projects</a>
         <a href="">Contact</a>
-      </nav>
+      </nav>}
+
     </div>
   )
 }
