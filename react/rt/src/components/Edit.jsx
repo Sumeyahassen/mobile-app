6be6c38b434a6fcea51b346bd7d198c46425b03a
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-25 16:03:06 +0300

    the the add buttone to work properly

--- react/rt/src/components/Edit.jsx
+++ react/rt/src/components/Edit.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React from "react";
+import Input from "./Input";
+function Edit({ tasks }) {
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((__, i) => i !== index);
+    setTask(newTasks);
+  };
+  const editTask = (index) => {
 
-function Edit() {
+  };
   return (
-    <div className='flex justify-center items-center gap-4 text-white py-5 font-bold'>
-      <button className="bg-yellow-500 px-8 py-3 rounded-lg font-bold">E</button>
-      <button className="bg-red-500 px-8 py-3 rounded-lg font-bold">D</button>
-      <button className="bg-green-500 px-8 py-3 rounded-lg font-bold">Y</button>
+    <div className="flex flex-col ">
+      {tasks.map((task, index) => (
+        <div className="flex justify-center items-center gap-4 text-white py-5 font-bold" key={index}>
+          <div className="text-black" key={index}>
+            {task}
+          </div>
+          <button
+            className="bg-yellow-500 px-8 py-3 rounded-lg font-bold"
+            onClick={editTask}
+          >
+            E
+          </button>
+          <button
+            onClick={() => deleteTask(index)}
+            className="bg-red-500 px-8 py-3 rounded-lg font-bold"
+          >
+            D
+          </button>
+          <button
+            onClick={editTask}
+            className="bg-green-500 px-8 py-3 rounded-lg font-bold"
+          >
+            Y
+          </button>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Edit
+export default Edit;
--- react/rt/src/components/Input.jsx
+++ react/rt/src/components/Input.jsx
@@ -1,26 +1,36 @@
-import React, { useState } from 'react'
-import Edit from './Edit'
+import React, { useState } from "react";
+import Edit from "./Edit";
 
-const [Task,setTask]=useState("")
 function Input() {
-    const addTask=()=>{
-        
+  const [Task, setTask] = useState(["day one"]); // for total tasks
+  const [newTask, setNewTask] = useState(""); // for the add button to clike add task
 
+  const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTask([...Task, newTask]);
+      setNewTask("");
     }
+  };
   return (
-   <div className="">
-     <div className=' flex justify-center items-center pt-10 gap-3'>
-      <input type="text"
-      value={setTask}
-      placeholder='Add To Day Task'
-      className='border-2 py-3 rounded-sm border-amber-700  w-1/3 placeholder:text-center' />
-      <button
-      className='bg-green-500 text-white px-16 py-3 rounded-lg'
-      onClick={addTask} >Add Task</button>
+    <div className="">
+      <div className=" flex justify-center items-center pt-10 gap-3">
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Add To Day Task"
+          className="border-2 py-3 rounded-sm border-amber-700  w-1/3 placeholder:text-center"
+        />
+        <button
+          className="bg-green-500 text-white px-16 py-3 rounded-lg rounded-br-sm"
+          onClick={addTask}
+        >
+          Add Task
+        </button>
+      </div>
+      <Edit tasks={Task} />
     </div>
-    <Edit/>
-   </div>
-  )
+  );
 }
 
-export default Input
+export default Input;
