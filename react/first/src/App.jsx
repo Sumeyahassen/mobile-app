23ef8adf54eeb6b98ca4ad96864585fca31f51a2
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-26 20:09:11 +0300

    to learn to crate componet in one file and how to render

--- react/first/src/App.jsx
+++ react/first/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+const Title=()=>{
+  return(
+    <div className="">
+      title component
+    </div>
+  )
+}
 const App = () => {
   return (
     <div className='bg-red-500'>
-      welcame
+      <Title/>
+      <Title/>
     </div>
   )
 }
