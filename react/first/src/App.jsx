61a225150c66c2cc1afede5a8608818480ae9583
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-26 20:16:19 +0300

    to learn  one component many time to  tage in the render all thing that component contant is  to desplay   for  repition time

--- react/first/src/App.jsx
+++ react/first/src/App.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 const Title=()=>{
+  console.log("title component renderd")
   return(
     <div className="">
       title component
     </div>
   )
 }
+const Description=()=>{
+  console.log("description component renderd")
+return(
+  <div className="">
+    it is the first  react app developemnt class
+  </div>
+)
+}
 const App = () => {
   return (
     <div className='bg-red-500'>
       <Title/>
       <Title/>
+      <Description/>
     </div>
   )
 }
