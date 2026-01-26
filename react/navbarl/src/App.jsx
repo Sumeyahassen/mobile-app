b5e0e1aa8fda55a168e938cc4f460d44319c7d01
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-26 19:56:17 +0300

    to start the the resact learning jorni

--- react/navbarl/src/App.jsx
+++ react/navbarl/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const name=prompt("Enter your Name: ")
 
   return (
-    <>
-     <div className='text-red-500'>sumeya</div>
-    </>
+    <div className='min-h-screen'>
+     {/* <div className='text-red-500'>your Name is {name}</div> */}
+     <div className='flex items-center justify-between gap-10 bg-slate-900 text-slate-50 '>
+      {/* for logo */}
+      <div>logo</div>
+      {/* for pages */}
+      <div className=' flex flex-col  justify-end '>
+        <span>Home</span>
+        <span>About</span>
+        <span>Servise</span>
+        <span>Contact</span>
+      </div>
+     </div>
+    </div>
   )
 }
 
