db0d53a23611c39fa56fad389a066f46adea8436
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-27 21:48:24 +0300

    to add interactive  desine  using tailwind css

--- react/first/src/App.jsx
+++ react/first/src/App.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import Student  from './components/Student'
 function App() {
   return (
-    <>
+    <div className="flex gap-4 justify-center items-center pt-10">
     <Student name="Sumeya" age={23} isStudent={true}/>
+    <Student name="Hikma" age={26} isStudent={false}/>
+    <Student name="Iman" age={22} isStudent={true}/>
+    <Student name="Biniyam" age={23} isStudent={true}/>
 
-    </>
+    </div>
   )
 }
 
--- react/first/src/components/Student.jsx
+++ react/first/src/components/Student.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Student(props) {
     // to practes react propes
   return (
-    <div>
+    <div className='font-sans  h-1/4 w-1/5 flex flex-col  justify-between bg-gray-800 text-white  text-center py-10 rounded-lg'>
 <p className="">
   Name:{props.name}
 </p>
