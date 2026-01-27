3bb232232e50dcbbafbcdad27943b1c9959407ad
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-27 14:28:18 +0300

    to include the propase

--- react/first/src/App.jsx
+++ react/first/src/App.jsx
@@ -1,38 +1,11 @@
 import React from 'react'
-const Title=(props)=>{
-  // props is in component is the agriment of this function
-  console.log("title component renderd")
-  return(
-    <div className="">
-      title component
-    </div>
-  )
-}
-const Description=()=>{
-  console.log("description component renderd")
-return(
-  <div className="">
-    it is the first  react app developemnt class
-  </div>
-)
-}
-const Message=(props)=>{
-  console.log(props);
-  const {text,number,boole}=props;
-  return <div className={`${text} `}>
-welcame to message compnent
-  </div>
-}
-const App = () => {
-  // the main component are parent
+import Student  from './components/Student'
+function App() {
   return (
-    <div className=''>
-      {/* render component arae  chiled  */}
-      {/* <Title/>
-      <Title/>
-      <Description/> */}
-      <Message text="bg-red-600" number={12} boole={false}/>
-    </div>
+    <>
+    <Student name="Sumeya"/>
+
+    </>
   )
 }
 
---
+++ react/first/src/components/Student.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Student(props) {
+    // to practes react propes
+  return (
+    <div>
+<p className="">
+  Name:{props.name}
+</p>
+    </div>
+  )
+}
+
+export default Student
