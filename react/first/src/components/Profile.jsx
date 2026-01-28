d627414b302223a3d1f1fb7e5fb1dadad815c089
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-28 17:08:52 +0300

    to learn how to include css inside the js

--- react/first/src/components/Profile.jsx
+++ react/first/src/components/Profile.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 function Profile({firstName,lastName,age}) {
+    const style={
+
+        color:"red",
+        fontSize:24
+    }
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col ' style={style}>
         <p className="">Full Name: {firstName} {lastName}</p>
         <p className="">Age:{age}</p>
 
