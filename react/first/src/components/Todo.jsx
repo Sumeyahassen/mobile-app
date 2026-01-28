e16b8c5cdb10266e603cfb301e47138f959d4fd2
sumeyahassen <sumeyahassensumi@gmail.com>
2026-01-28 23:39:29 +0300

    to start project two usestate

---
+++ react/first/src/components/Todo.jsx
@@ -0,0 +1,17 @@
+// Functionality:
+// Add new todo items
+// Mark as complete/incomplete
+// Delete items
+// Show total/completed count
+// Filter (all/active/completed)
+import React from 'react'
+
+function Todo() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Todo
