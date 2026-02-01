483d9c1f1fa324d09c6dce2c478b3bfc2e65b68d
sumeyahassen <sumeyahassensumi@gmail.com>
2026-02-01 21:35:24 +0300

    to the api contant desplay

--- react/first/src/components/Api.jsx
+++ react/first/src/components/Api.jsx
@@ -3,11 +3,12 @@ import React, { useEffect, useState } from "react";
 
 function Api() {
   const [resoursetype, setresoursetype] = useState("posts");
+  const [item,setItem]=useState([]);
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${resoursetype}`)
       .then((response) => response.json())
-      .then((json) => console.log(json));
-  });
+      .then((json) => setItem(json));
+  },[resoursetype]);
   return (
     <div className="flex flex-col">
       <button onClick={() => setresoursetype("posts")} className="">
@@ -20,6 +21,11 @@ function Api() {
         comments
       </button>
       {resoursetype}
+      <pre>
+        {/* json object to  string  to use all in capital */}
+        {JSON.stringify(item)}
+        <br />
+      </pre>
     </div>
   );
 }
