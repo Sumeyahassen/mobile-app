e394a93ef6750d24a863266d6a9361f05d1d6655
sumeyahassen <sumeyahassensumi@gmail.com>
2026-02-02 21:39:22 +0300

    to modifay the respons of the header

--- react/apple/src/components/Header.jsx
+++ react/apple/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FaSearch, FaApple } from "react-icons/fa";
 import { IoBag } from "react-icons/io5";
 import { ImMenu } from "react-icons/im";
+import { IoIosClose } from "react-icons/io";
 function Header() {
   const headers = [
     "store",
@@ -27,18 +28,27 @@ function Header() {
             </a>
           );
         })}
-        <FaSearch className="text-gray-500" />
-        <IoBag className="text-gray-500" />
+        <FaSearch className="text-gray-500" size={20} />
+        <IoBag className="text-gray-500" size={20} />
       </div>
       {/* mobile view */}
-      <div className="flex justify-between lg:hidden px-10">
-        <FaApple className="text-gray-800" />
-        <div className="flex gap-5">
-          <FaSearch className="text-gray-500" />
-          <IoBag className="text-gray-500" />
-          <button onClick={() => setOpen} className=" lg:hidden">
-            <ImMenu />
-          </button>
+
+      <div className="flex justify-between items-center py-3 px-5 ">
+        <FaApple />
+        {isOpen &&
+          headers.map((link, index) => {
+            <a className="flex flex-col h-auto w-full" key={index}>
+              {link}
+            </a>;
+          })}
+        <div className="flex justify-center gap-5">
+          <FaSearch />
+          <IoBag />
+         <button
+         onClick={()=>setOpen(!isOpen)}
+         >
+            {isOpen?<IoIosClose/>:<ImMenu/>}
+         </button>
         </div>
       </div>
     </div>
