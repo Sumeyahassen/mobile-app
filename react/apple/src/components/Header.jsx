29065d868c9f879efa277b30f420d0a303b5dc5a
sumeyahassen <sumeyahassensumi@gmail.com>
2026-02-02 20:34:50 +0300

    to reaponse the heaader section

--- react/apple/src/components/Header.jsx
+++ react/apple/src/components/Header.jsx
@@ -1,23 +1,48 @@
-import React from 'react'
-import { FaSearch,FaApple } from 'react-icons/fa'
+import React, { useState } from "react";
+import { FaSearch, FaApple } from "react-icons/fa";
 import { IoBag } from "react-icons/io5";
+import { ImMenu } from "react-icons/im";
 function Header() {
-    const headers=[<FaApple className='text-gray-800'/>,
-        "store","mack","ipad","iphone","watch","vision","airpod","tv&home","entertament","support",
-        <FaSearch className='text-gray-500'/>,<IoBag className='text-gray-500'/>]
+  const headers = [
+    "store",
+    "mack",
+    "ipad",
+    "iphone",
+    "watch",
+    "vision",
+    "airpod",
+    "tv&home",
+    "entertament",
+    "support",
+  ];
+  const [isOpen, setOpen] = useState(false);
   return (
     <div>
-        <div className="flex justify-center items-center gap-7 py-5 font-medium bg-gray-50 text-gray-900">
-            {headers.map((link,index)=>{
-                return (
-                <a key={index}>{link}</a>
-                )
-            })}
+      <div className="hidden lg:flex justify-center items-center gap-10 py-3 bg-gray-50 text-gray-950">
+        <FaApple className="text-gray-800" />
+        {headers.map((link, index) => {
+          return (
+            <a href="#" key={index}>
+              {link}
+            </a>
+          );
+        })}
+        <FaSearch className="text-gray-500" />
+        <IoBag className="text-gray-500" />
+      </div>
+      {/* mobile view */}
+      <div className="flex justify-between lg:hidden px-10">
+        <FaApple className="text-gray-800" />
+        <div className="flex gap-5">
+          <FaSearch className="text-gray-500" />
+          <IoBag className="text-gray-500" />
+          <button onClick={() => setOpen} className=" lg:hidden">
+            <ImMenu />
+          </button>
         </div>
-
+      </div>
     </div>
-  )
+  );
 }
 
-
-export default Header
+export default Header;
