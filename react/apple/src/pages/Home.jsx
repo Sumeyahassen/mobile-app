681391794834a613eff544e3b8ddc0297b121dc0
sumeyahassen <sumeyahassensumi@gmail.com>
2026-02-04 23:10:11 +0300

    to learn in the component is full scree is avelave for that componenet

--- react/apple/src/pages/Home.jsx
+++ react/apple/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../components/Header'
 import MackPro from "../components/MackPro"
 import MackBook from '../components/MackBook'
+import Iphone from '../components/Iphone'
 
 function Home() {
   return (
     <div>
         <Header/>
         <MackBook/>
+        <Iphone/>
         {/* <MackPro/> */}
 
     </div>
