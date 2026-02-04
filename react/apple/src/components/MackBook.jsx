5ffab0b122fde7c93a035fe47f8c94a633544ea7
sumeyahassen <sumeyahassensumi@gmail.com>
2026-02-04 22:32:22 +0300

    to include the mack book pro  component

---
+++ react/apple/src/components/MackBook.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MackBook() {
+  return (
+    <div>
+      <div className="text-gray-500">the next world of app</div>
+      <div className="text-2xl text-black">Mack Book Pro</div>
+      <div className="">The best for the briteness</div>
+      <div className="">
+        <a href="">detale</a>
+        <a href="">more</a>
+      </div>
+    </div>
+  )
+}
+
+export default MackBook
