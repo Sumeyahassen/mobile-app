1b487e8b07bb6286f7360b2e402d132115d5b67b
sumeyahassen <sumeyahassensumi@gmail.com>
2026-02-04 22:48:14 +0300

    to respose  the text in the lagest text and in the code to add the right icone

--- react/apple/src/components/MackBook.jsx
+++ react/apple/src/components/MackBook.jsx
@@ -4,7 +4,7 @@ function MackBook() {
   return (
     <div className=' flex flex-col  items-center py-10 bg-gray-300 h-screen '>
       <div className="text-gray-500 pt-10 text-xl capitalize">the next world of app</div>
-      <div className="text-6xl text-black font-bold p-4">Mack Book Pro</div>
+      <div className="text-3xl sm:text-6xl text-black font-bold p-4">Mack Book Pro</div>
       <div className="">The best for the briteness</div>
       <div className="flex gap-3">
         <a href="" className='flex justify-center items-center'>detale <IoIosArrowForward/></a>
